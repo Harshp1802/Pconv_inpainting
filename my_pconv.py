05d11fc3f43395b82f8eb66720f801976f284ee8
shivam15s <shivam15800@gmail.com>
2021-03-31 21:14:45 +0530

    pconv & unet initial commit

---
+++ my_pconv.py
@@ -0,0 +1,51 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class PConv(nn.Module):
+    def __init__(self, input_channels, output_channels, kernel_size = (3, 3), stride = (1, 1), dilation = 1, padding = 0, bias = True):
+        super().__init__()
+        
+        # Create a 2D convolutional layer for the images of the partial convolution.
+        self.image_convolution = nn.Conv2d(input_channels, output_channels, kernel_size, stride, padding, dilation, bias = True)
+
+        # Initialise Gaussian Weights for Image_Convolution Kernel with mean 0, standard deviation 0.02
+        torch.nn.init.normal(self.image_convolution.weight, 0.0, 0.02) 
+
+        # Create a 2D convolutional layer for the masks of the partial convolution.
+        self.mask_convolution = nn.Conv2d(input_channels, output_channels, kernel_size, stride, padding, dilation, bias = False)
+        # Initialise Constant Weights of value for Mask_Convolution Kernel
+        torch.nn.init.constant_(self.mask_convolution, 1.0)
+
+        # We don't want the masks to get updated through gradients since that is what we will do manually
+        for param in self.mask_convolution.parameters():
+            param.requires_grad = False
+
+    def forward(self, image, mask):
+
+        # Convolve only at places where the mask is set
+        output = self.image_convolution(image * mask)
+
+        # We include a bias term
+        bias = self.image_convolution.bias.view(1,-1,1,1).expand_as(output)
+        
+        # We will use the mask convolution layer defined earlier to obtain the 2D mask for the output.
+        # However, we do not want any gradients to be computed, since we will be doing that manually in the next few lines
+        with torch.no_grad():
+            output_mask = self.mask_convolution(mask)
+
+        # Those places become holes where the output mask is not set
+        no_update_holes = (output_mask == 0)
+        # Temporarily filling up hole regions with ones so that we do not face any errors during element by element division done in the next step
+        mask_normaliser = output_mask.masked_fill_(no_update_holes, 1.0)
+
+        # Since we are using Partial Convolutions, normalising the mask only over the no-update region is necessary
+        output = ((output - output_bias) / mask_normaliser + output_bias).masked_fill_(no_update_holes, 0.0)
+
+        # Now the new mask will be generated using the no_update_holes
+        new_mask = (torch.ones_like(output)).masked_fill_(no_update_holes)
+
+        # The output, along with the updated mask, goes on to the next layer
+        return output, new_mask
